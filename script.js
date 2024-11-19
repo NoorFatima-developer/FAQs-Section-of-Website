7f2305ebdc99237f14fbce721d3417c590e6b123
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-19 12:10:56 +0500

    code is added

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ accordion.forEach(accordian => {
     const icon = accordian.querySelector('.icon');
     const answer = accordian.querySelector('.answers');
 
+    icon.addEventListener('click', () => {
+        icon.classList.toggle('active');
+        answer.classList.toggle('active');
+        
+    })
+
 })
