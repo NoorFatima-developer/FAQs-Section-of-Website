9d83a08fe41f02405ea325d486fef04e872913d2
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-19 10:45:22 +0500

    code is added

---
+++ script.js
@@ -0,0 +1,7 @@
+const accordian = document.querySelectorAll('.accordian');
+
+accordian.forEach(accordian => {
+    const icon = document.querySelector('.icon');
+    const answer = document.querySelector('.answers');
+
+})
