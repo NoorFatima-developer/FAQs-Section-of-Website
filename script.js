0867e31333e06a930a920eeeebed8fbce7af7604
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-19 12:22:39 +0500

    code is added

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ accordion.forEach(accordian => {
     const answer = accordian.querySelector('.answers');
 
     icon.addEventListener('click', () => {
-        icon.classList.toggle('active');
-        answer.classList.toggle('active');
-        
-    })
+        // icon.classList.toggle('active');
+        // answer.classList.toggle('active');
 
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }
+        else{
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';    //scroll gives integer value, that's why use px, it will convert value to integer...
+        }
+       
+    })
 })
